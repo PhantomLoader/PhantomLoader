e3d391b809ef99dc529475df16150b50b920ed68
Nico <nico.hex6@gmail.com>
2023-12-15 16:52:27 +0100

    Initial commit

---
+++ example/src/main/java/io/github/hexagonnico/Example.java
@@ -0,0 +1,11 @@
+package io.github.hexagonnico;
+
+import io.github.hexagonnico.absload.ModInitializer;
+
+public class Example {
+
+	@ModInitializer("example")
+	public static void initializeMod() {
+
+	}
+}
---
+++ library-common/src/main/java/io/github/hexagonnico/absload/ModInitializer.java
@@ -0,0 +1,6 @@
+package io.github.hexagonnico.absload;
+
+public @interface ModInitializer {
+
+	String value();
+}
