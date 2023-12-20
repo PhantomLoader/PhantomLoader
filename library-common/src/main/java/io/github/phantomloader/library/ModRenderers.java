c99f1f50fe0255c19e563241c26573ead77e0e11
Nico <nico.hex6@gmail.com>
2023-12-20 22:13:13 +0100

    Added renderers registries

---
+++ library-common/src/main/java/io/github/phantomloader/library/ModRenderers.java
@@ -0,0 +1,86 @@
+package io.github.phantomloader.library;
+
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+import java.util.HashMap;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+/**
+ * <p>
+ *     Static class used to store {@link Entity} and {@link BlockEntity} renderers.
+ * </p>
+ * <p>
+ *     Renderers may be added by calling {@link ModRenderers#registerEntityRenderer(Supplier, EntityRendererProvider)} and {@link ModRenderers#registerBlockEntityRenderer(Supplier, BlockEntityRendererProvider)} from the common module.
+ *     The Phantom library will then query those values for each mod loader to register renderers.
+ * </p>
+ *
+ * @author Nico
+ */
+public class ModRenderers {
+
+	/** Entities to register */
+	private static final HashMap<Supplier<?>, EntityRendererProvider<?>> ENTITIES = new HashMap<>();
+	/** Block entities to register */
+	private static final HashMap<Supplier<?>, BlockEntityRendererProvider<?>> BLOCK_ENTITIES = new HashMap<>();
+
+	/**
+	 * <p>
+	 *     Registers an {@link net.minecraft.client.renderer.entity.EntityRenderer}.
+	 * </p>
+	 *
+	 * @param entity A supplier returning the registered entity type, the one returned by {@link ModRegistry#registerEntity(String, EntityType.Builder)}.
+	 * @param renderer The entity renderer's constructor passed as a method reference.
+	 * @param <T> The entity's class.
+	 */
+	public static <T extends Entity> void registerEntityRenderer(Supplier<EntityType<T>> entity, EntityRendererProvider<T> renderer) {
+		ENTITIES.put(entity, renderer);
+	}
+
+	/**
+	 * <p>
+	 *     Registers a {@link net.minecraft.client.renderer.blockentity.BlockEntityRenderer}.
+	 * </p>
+	 *
+	 * @param blockEntity A supplier returning the registered block entity type, the one returned by {@link ModRegistry#registerBlockEntity(String, BiFunction, Supplier)}.
+	 * @param renderer The block entity renderer's constructor passed as a method reference.
+	 * @param <T> The block entity's class.
+	 */
+	public static <T extends BlockEntity> void registerBlockEntityRenderer(Supplier<BlockEntityType<T>> blockEntity, BlockEntityRendererProvider<T> renderer) {
+		BLOCK_ENTITIES.put(blockEntity, renderer);
+	}
+
+	/**
+	 * <p>
+	 *     Queries all entity renderers added with {@link ModRenderers#registerEntityRenderer(Supplier, EntityRendererProvider)} and registers them.
+	 *     Called from the loader-specific modules of the Phantom library.
+	 * </p>
+	 *
+	 * @param consumer Registry function.
+	 * @param <T> The entity's class.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T extends Entity> void registerEntityRenderers(BiConsumer<EntityType<T>, EntityRendererProvider<T>> consumer) {
+		ENTITIES.forEach((entity, renderer) -> consumer.accept((EntityType<T>) entity.get(), (EntityRendererProvider<T>) renderer));
+	}
+
+	/**
+	 * <p>
+	 *     Queries all block entity renderers added with {@link ModRenderers#registerBlockEntityRenderer(Supplier, BlockEntityRendererProvider)} and registers them.
+	 *     Called from the loader-specific modules of the Phantom library.
+	 * </p>
+	 *
+	 * @param consumer Registry function.
+	 * @param <T> The block entity's class.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T extends BlockEntity> void registerBlockEntityRenderers(BiConsumer<BlockEntityType<? extends T>, BlockEntityRendererProvider<T>> consumer) {
+		BLOCK_ENTITIES.forEach((blockEntity, renderer) -> consumer.accept((BlockEntityType<? extends T>) blockEntity.get(), (BlockEntityRendererProvider<T>) renderer));
+	}
+}
--- library-fabric/src/main/java/io/github/phantomloader/library/fabric/FabricClientInitializer.java
+++ library-fabric/src/main/java/io/github/phantomloader/library/fabric/FabricClientInitializer.java
@@ -1,18 +1,37 @@
 package io.github.phantomloader.library.fabric;
 
 import io.github.phantomloader.library.CreativeTabs;
+import io.github.phantomloader.library.ModRenderers;
 import io.github.phantomloader.library.fabric.renderers.BlockEntityItemRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.world.level.block.Block;
 
 import java.util.HashSet;
+import java.util.function.Supplier;
 
+/**
+ * <p>
+ *     Fabric client mod initializer.
+ * </p>
+ *
+ * @author Nico
+ */
 public class FabricClientInitializer implements ClientModInitializer {
 
 	private static final HashSet<Block> ENTITY_BLOCKS = new HashSet<>();
 
+	/**
+	 * <p>
+	 *     Registers a Fabric block entity renderer.
+	 *     Called from {@link FabricRegistry#registerBlockItem(String, Supplier)}.
+	 * </p>
+	 *
+	 * @param block The entity block.
+	 */
 	public static void registerEntityBlockRenderer(Block block) {
 		ENTITY_BLOCKS.add(block);
 	}
@@ -21,5 +40,7 @@ public class FabricClientInitializer implements ClientModInitializer {
 	public void onInitializeClient() {
 		ENTITY_BLOCKS.forEach(block -> BuiltinItemRendererRegistry.INSTANCE.register(block, new BlockEntityItemRenderer(block)));
 		CreativeTabs.allTabs().forEach(resourceKey -> ItemGroupEvents.modifyEntriesEvent(resourceKey).register(listener -> CreativeTabs.accept(resourceKey, supplier -> listener.accept(supplier.get()))));
+		ModRenderers.registerEntityRenderers(EntityRendererRegistry::register);
+		ModRenderers.registerBlockEntityRenderers(BlockEntityRenderers::register);
 	}
 }
